9665cf37c9c02193cfd80bf649411613fcab4b2b
Denis <dazzkeruyakota5985@gmail.com>
2023-10-04 20:44:22 +0400

    Add migrations.

--- SoftLineTest.DAL/ApplicationDbContext.cs
+++ SoftLineTest.DAL/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ namespace SoftLineTest.DAL
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext()
         {
 
         }
